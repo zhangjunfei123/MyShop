f6da2046275912f663a82a799585b2ff5de6d8e9
Administrator <Administrator@stu77531b>
2019-03-07 15:36:14 +0800

    genggai

--- WebApplication4/Controllers/UserController.cs
+++ WebApplication4/Controllers/UserController.cs
@@ -13,5 +13,9 @@ namespace WebApplication4.Controllers
         {
             return View();
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
     }
 }
